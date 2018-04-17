bd6ef43d924813ee4267832e582d61a7f4bddb43
Vadim Developer <netuns@gmail.com>
2018-04-17 00:33:28 -0400

    Zooming

--- src/main/resources/static/js/result-processing.js
+++ src/main/resources/static/js/result-processing.js
@@ -1,6 +1,7 @@
 $(document).ready(function (){
     var jobId = $('#jobId').text();
     getIfReady(jobId);
+
 });
 
 
@@ -24,38 +25,58 @@ function tryToGetFileName(jobId) {
 }
 
 function processRetrievedDataAsync(data) {
+
+//
+//    drag = d3.behavior.drag()
+//        .origin(function(d) { return d; })
+//        .on("dragstart", dragstarted)
+//        .on("drag", dragged)
+//        .on("dragend", dragended);
+
     if (data.status[0] != 'notReady') {
         clearInterval(fileGetter);
 
         if (data.result.length === 1) {
             $('#results-load').attr('href', data.result[0]);
         }
-
         if (data.result.length > 1) {
             $('#results-load').attr('href', data.result[1]);
-            d3.select("div#svgContainer")
+            var tree = d3.select("div#svgContainer")
             .append("svg")
-            .attr("class", "bioTree")
             .attr("width", 1000)
-            .attr("height", 1200);
-
-            d3.select('svg.bioTree')
+            .attr("height", 1200)
+            .style("pointer-events", "all")
+            .call(d3.zoom().on("zoom", function() {tree.attr("transform", d3.event.transform)}))
             .append("g")
-            .attr("id", "treeContainer")
-            .attr("transform", "translate(0, 0)");
-
-
-
+            .attr("id", "treeContainer");
 
             d3.xml(data.result[1]).then(function(xml) {
                 console.log(xml.documentElement)
                 document.getElementById("treeContainer").appendChild(xml.documentElement);
             });
+
         }
         $('.result-container').show();
-
 	}
+
 }
+
+
+
+
+function dragstarted(d) {
+  d3.event.sourceEvent.stopPropagation();
+  d3.select(this).classed("dragging", true);
+}
+
+function dragged(d) {
+  d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+}
+
+function dragended(d) {
+  d3.select(this).classed("dragging", false);
+}
+
 
 function error(jqXHR, textStatus, errorThrown) {
 	window.alert('Error happened!');
