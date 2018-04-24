63c6908c536b818282aa1c003b72a33aca8ccd44
Vadim Developer <netuns@gmail.com>
2018-04-24 00:37:17 -0400

    Took care of styles. Setting the width of the rectangular container in advance.

--- src/main/resources/static/js/result-processing.js
+++ src/main/resources/static/js/result-processing.js
@@ -1,6 +1,12 @@
 $(document).ready(function (){
-    var jobId = $('#jobId').text();
+    var minSvgWidth = 650;
+    var widthShrinkageFactor = 0.8;
+    var heightShrinkageFactor = 0.8;
+    var width = window.innerWidth*widthShrinkageFactor;
+    var height = window.innerHeight*heightShrinkageFactor;
+    treeContainer = prepareTreeContainer(width, height);
 
+    var jobId = $('#jobId').text();
     getIfReady(jobId);
 
 });
@@ -9,7 +15,9 @@ function getIfReady(jobId) {
     console.log('Checking if ready ');
 
     console.log('jobId ' + jobId + Cookies.get('protoTree'));
-    fileGetter = setInterval(function() {tryToGetFileName(jobId + "-" + Cookies.get('protoTree'))}, 5000);
+    fileGetter = setInterval(function() {
+        tryToGetFileName(jobId + "-" + Cookies.get('protoTree'))
+    }, 5000);
 }
 
 function tryToGetFileName(jobId) {
@@ -24,13 +32,20 @@ function tryToGetFileName(jobId) {
     });
 }
 
-function processRetrievedDataAsync(data) {
-    var minSvgWidth = 650;
-    var widthShrinkageFactor = 0.8;
-    var heightShrinkageFactor = 0.8;
-    var width = window.innerWidth*widthShrinkageFactor;
-    var height = window.innerHeight*heightShrinkageFactor;
-
+function prepareTreeContainer(width, height) {
+    var tree = d3.select("div#svgContainer")
+        .style("width", width)
+        .style("height", height)
+        .style("border", "1px solid #9494b8")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style("pointer-events", "all")
+        .call(d3.zoom().on("zoom", function() {tree.attr("transform", d3.event.transform)}))
+        .append("g")
+        .attr("id", "treeContainer");
+}
+function processRetrievedDataAsync(data, treeContainer, width, height) {
     if (data.status[0] != 'notReady') {
         clearInterval(fileGetter);
 
@@ -39,18 +54,6 @@ function processRetrievedDataAsync(data) {
         }
         if (data.result.length > 1) {
             $('#results-load').attr('href', data.result[1]);
-            var tree = d3.select("div#svgContainer")
-            .style("width", width)
-            .style("height", height)
-            .style("border", "1px solid #9494b8")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .style("pointer-events", "all")
-            .call(d3.zoom().on("zoom", function() {tree.attr("transform", d3.event.transform)}))
-            .append("g")
-            .attr("id", "treeContainer");
-
             d3.xml(data.result[1]).then(function(xml) {
                 console.log(xml.documentElement)
                 document.getElementById("treeContainer").appendChild(xml.documentElement);
