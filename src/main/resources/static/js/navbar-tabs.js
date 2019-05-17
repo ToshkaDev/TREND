d02ad733f34741f3a45d67b72a88144e604dd4a8
Vadim Gumerov <netuns@gmail.com>
2019-05-17 13:16:37 -0400

    Favicon added, change the height of footer on window resize, correced a function name: new name adjustStickyNavbar()

--- src/main/resources/static/js/navbar-tabs.js
+++ src/main/resources/static/js/navbar-tabs.js
@@ -4,7 +4,7 @@ $(document).ready( function() {
 	var mainTab = "#" + $('#mainTab').text();
 	mainNavbar(mainTab);
 	// When the user scrolls the page, execute myFunction
-    window.onscroll = function() {myFunction()};
+    window.onscroll = function() {adjustStickyNavbar()};
 });
 
 function mainNavbar(tab) {
@@ -29,7 +29,7 @@ navbar = document.getElementById("navbar");
 // Get the offset position of the navbar
 sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function adjustStickyNavbar() {
 
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
