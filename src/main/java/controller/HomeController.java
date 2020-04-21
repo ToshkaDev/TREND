0d2f6726b36450c6dfabd863cf563a5d2a01f266
Vadim Developer <netuns@gmail.com>
2020-04-21 16:33:12 -0400

    Add ProTips section and start populating it. Some little changes

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -13,20 +13,27 @@ public class HomeController {
     public String home(Model model) {
         model.addAttribute("mainTab", "home");
         return "main-view  :: addContent(" +
-                "fragmentsMain='help-and-about', help='about')";
+                "fragmentsMain='help-and-about', infoFragment='about')";
     }
 
     @GetMapping(value={"/help"})
     public String help(Model model) {
         model.addAttribute("mainTab", "help");
         return "main-view  :: addContent(" +
-                "fragmentsMain='help-and-about', help='help')";
+                "fragmentsMain='help-and-about', infoFragment='help')";
+    }
+
+    @GetMapping(value={"/protips"})
+    public String proTips(Model model) {
+        model.addAttribute("mainTab", "protips");
+        return "main-view  :: addContent(" +
+                "fragmentsMain='help-and-about', infoFragment='protips')";
     }
 
     @GetMapping(value={"/contact"})
     public String contact(Model model) {
         model.addAttribute("mainTab", "contact");
         return "main-view  :: addContent(" +
-                "fragmentsMain='help-and-about', help='contact')";
+                "fragmentsMain='help-and-about', infoFragment='contact')";
     }
 }
